12e37d8008844b7063282df00f328342160fd0aa
Joel Llacsahuache Copia <=joel.llacsahuache@upeu.edu.pe>
2024-01-12 05:02:01 -0500

    Modificando formulario

--- js/inicio.js
+++ js/inicio.js
@@ -11,6 +11,8 @@ document.getElementById('formulario').addEventListener('submit',
 
         event.preventDefault();//evita que el formulario se envie automaticamente
 
+
+        var entidad = document.getElementById('entidad').value;
         var tipoMensaje = document.getElementById('tipoMensaje').value;
         var nombresCom = document.getElementById('cliente').value;
         var fecha = document.getElementById('fecha').value;
@@ -18,13 +20,12 @@ document.getElementById('formulario').addEventListener('submit',
         var monto = document.getElementById('monto').value;
         var tipo = document.getElementById('tipo').value;
         var numero = document.getElementById('numero').value;
-        var idCliente = document.getElementById('idCliente').value;
 
         var nombresMayus = nombresCom.toUpperCase();
         var tipoMayus = tipo.toUpperCase();
 
         var cliente = {
-            clienteId: idCliente,
+            entidad: entidad,
             tipoMensaje: tipoMensaje,
             nombres: nombresMayus,
             fechaPago: fecha,
@@ -37,54 +38,38 @@ document.getElementById('formulario').addEventListener('submit',
         generarMensaje(cliente);
     });
 function generarMensaje(cliente) {
-    limpiarCampos();
+
     mostrarMensaje(cliente);
 
 }
 
-function limpiarCampos() {
-    mensajeMostrar.innerHTML = "";
-}
-
-function limpiarCamposTabla() {
-    codCliente.innerHTML = "";
-    telf.innerHTML = "";
-    fechaComp.innerHTML = "";
-    descrip.innerHTML = "";
-    tipoGestion.innerHTML = "";
-    monto.innerHTML = "";
-}
-function llenarTabla(cliente) {
-    limpiarCamposTabla();
-    codCliente.innerHTML = cliente.clienteId;
-    telf.innerHTML = cliente.telefono;
-    fechaComp.innerHTML = cliente.fechaPago;
-    monto.innerHTML = cliente.montoPago;
-    let whatsapp = document.getElementById('whatsapp')
-    let link = 'http://wa.me/51' + cliente.telefono
-    whatsapp.href = link
-    whatsapp.innerHTML = "Enviar WhatsApp"
-}
 var mensaje = "";
 function mostrarMensaje(cliente) {
-    llenarTabla(cliente);
-    var promesaInterbank = "Sr.(a) *" + cliente.nombres + "* Según lo acordado telefónicamente usted se compromete a efectuar el pago correspondiente de S/. *" + cliente.montoPago + "* el día *" + cliente.fechaPago + "* del producto *" + cliente.tipoProducto + "*  Número de cuenta *" + cliente.numeroProducto + "*.\n"
+    var iconoWhatsApp = document.getElementById('iconoWhatsApp');
+    if (cliente.telefono != null) {
+        iconoWhatsApp.style.display = "block";
+    } 
+    var linkWhatsapp = document.getElementById('linkWhatsApp');
+    var link = 'http://api.whatsapp.com/send?phone=51' + cliente.telefono
+    linkWhatsapp.href = link
+    var promesaInterbank = "Sr(a) *" + cliente.nombres + "* Según lo acordado telefónicamente usted se compromete a efectuar el pago correspondiente de S/. *" + cliente.montoPago + "* el día *" + cliente.fechaPago + "* del producto *" + cliente.tipoProducto + "*  Número de cuenta *" + cliente.numeroProducto + "*.\n"
         + "\nRealice el pago en la agencia del *Banco BBVA* más cercana, donde se dirige a ventanilla indicando el *código pago legal* 248 (Soles) o 249 (Dólares) adicionando el número de su DNI o RUC del titular de la cuenta."
         + "\n\nQuedo a la espera del envió del voucher a través de este medio, gracias."
-        + "\n\n\nCuenta bancaria"
+        + "\n\n\n*Cuenta bancaria*"
         + "\n001104024900000023 - (Soles)"
         + "\n01140200490000002359 (Soles)"
-        + "\n\n\nCuenta interbancaria"
+        + "\n\n\n*Cuenta interbancaria*"
         + "\n001104024900000031 - (Dolares)"
         + "\n01140200490000003152 (Dolares)";
-    var promesaVentanilla = "Sr.(a) *" + cliente.nombres + "* Según lo acordado telefónicamente usted se compromete a efectuar el pago correspondiente a S/. *" + cliente.montoPago + "* el día *" + cliente.fechaPago + "* del producto *" + cliente.tipoProducto + "*  Número de cuenta *" + cliente.numeroProducto + "*.\n"
+    var promesaVentanilla = "Sr(a) *" + cliente.nombres + "* Según lo acordado telefónicamente usted se compromete a efectuar el pago correspondiente a S/. *" + cliente.montoPago + "* el día *" + cliente.fechaPago + "* del producto *" + cliente.tipoProducto + "*  Número de cuenta *" + cliente.numeroProducto + "*.\n"
         + "\nRealice el pago en la agencia del *Banco BBVA* más cercana, donde se dirige a ventanilla indicando el *código pago legal* 248 (Soles) o 249 (Dólares) adicionando el número de su DNI o RUC del titular de la cuenta."
         + "\n\nQuedo a la espera del envió del voucher a través de este medio, gracias.";
-    var recordatorio = "Sr.(a) *" + cliente.nombres + "* le recordamos que tiene una promesa incumplida con el *Banco BBVA* por el monto de S/. *" + cliente.montoPago + "* .Para que Ud. no pierda la campaña y su deuda no genere mayor interés, confírmenos su reprogramación para el día de HOY. Gracias."
+    var vencida = "Sr(a) *" + cliente.nombres + "* le recordamos que tiene una promesa incumplida con   " + cliente.entidad + " por el monto de S/. *" + cliente.montoPago + "* .\n\nPara que Ud. no pierda los beneficios y su deuda no genere mayor interés, confírmenos su reprogramación para el día de *HOY*. \n\nGracias."
 
+    var recordatorio = "Que tal Sr(a) *" + cliente.nombres + "* , " + cliente.entidad + " le recuerda que tiene un compromiso pendiente para el *" + cliente.fechaPago + "* , por el importe de S/.*" + cliente.montoPago + "* , evite el recálculo de su deuda pagando en la fecha establecida. \n\nCualquier inconveniente con su pago me informa para poder ayudarle. \n\nQuedo a la espera de la foto de su comprobante para el ajuste de su pago. \nSaludos Cordiales.";
 
-    var campaña = cliente.nombres + ', *Banco BBVA* tiene un Dscto Especial *PRE_APROBADO*, cancela tu Producto *' + cliente.tipoProducto + '* con *S/.' +
-        cliente.montoPago + '* , Previa evaluación, \n*ACTIVALO* comunicándote por este medio.\n\n Tramite su *CONSTANCIA DE NO ADEUDO* y Evite seguir mantendiendo un reporte negativo en las centrales de riesgo.<br><br>Dscto válido hasta: *' + cliente.fechaPago + '*'
+    var campaña = cliente.nombres + ', *' + cliente.entidad + '*, tiene un Dscto Especial *PRE_APROBADO*, cancela tu Producto *' + cliente.tipoProducto + '* con *S/.' +
+        cliente.montoPago + '* , Previa evaluación, \n\n*ACTIVALO* comunicándote por este medio.\n\nTramite su *CONSTANCIA DE NO ADEUDO* y Evite seguir manteniendo un reporte negativo en las centrales de riesgo.\n\nDscto válido hasta: *' + cliente.fechaPago + '*'
     if (cliente.tipoMensaje == "promesa1") {
         mensaje = promesaVentanilla;
         mensajeMostrar.innerHTML = promesaVentanilla;
@@ -97,28 +82,49 @@ function mostrarMensaje(cliente) {
     } else if (cliente.tipoMensaje == "recordatorio") {
         mensaje = recordatorio;
         mensajeMostrar.innerHTML = recordatorio;
+    } else if (cliente.tipoMensaje == "vencida") {
+        mensaje = vencida;
+        mensajeMostrar.innerHTML = vencida;
     }
 }
+let btnCopiar = document.getElementById('copiarMensaje');
+btnCopiar.addEventListener('click', function (e) {
+    mensajeMostrar.select();
+    try {
+        var successful = document.execCommand('copy');
+
+        if (successful) respuesta.innerHTML = 'Copiado!';
+        else respuesta.innerHTML = 'Incapaz de copiar!';
+    } catch (err) {
+        rpta.innerHTML = 'Browser no soportado!';
+    }
+    let textArea = document.getElementById('mensaje')
+    textArea.innerHTML = ""
+});
 
 document.getElementById('tipoMensaje').addEventListener('change', function (event) {
     var opcionSeleccionada = event.target.value;//obtiene el valor de la opcion seleccionada
-
     var fechaDiv = document.getElementById('fechaDiv');
     var telefonoDiv = document.getElementById('telefonoDiv');
     var tipoProductoDiv = document.getElementById('tipoProductoDiv');
     var numeroProductoDiv = document.getElementById('numeroProductoDiv');
-    var codClienteDiv = document.getElementById('idClienteDiv');
 
     if (opcionSeleccionada == 'recordatorio') {
-        fechaDiv.style.display = 'none';
-        telefonoDiv.style.display = 'none';
+        fechaDiv.style.display = 'block';
         tipoProductoDiv.style.display = 'none';
         numeroProductoDiv.style.display = 'none';
 
-    } else if(opcionSeleccionada=="campaña"){
-        codClienteDiv.style.display = 'none';
-        numeroProductoDiv.style.display="none";
-    }else{
+    } else if (opcionSeleccionada == "campaña") {
+        fechaDiv.style.display = 'block';
+        numeroProductoDiv.style.display = "none";
+        telefonoDiv.style.display = 'block';
+        tipoProductoDiv.style.display = "none";
+    } else if (opcionSeleccionada == "vencida") {
+        fechaDiv.style.display = 'none';
+        numeroProductoDiv.style.display = "none";
+        telefonoDiv.style.display = 'block';
+        tipoProductoDiv.style.display = "none";
+    } else {
         fechaDiv.style.display = 'block';
         telefonoDiv.style.display = 'block';
         tipoProductoDiv.style.display = 'block';
